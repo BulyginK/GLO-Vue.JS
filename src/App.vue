edc1b7d03a76ac65454d3e72df13769f03576a77
BulyginK <bulygin.k@gmail.com>
2022-05-26 22:59:33 +0300

    rename

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
-  <div id="app">
-    Hello Vue.JS
-  </div>
+  <div id="app">Hello Vue.JS</div>
 </template>
 
 <script>
-
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
